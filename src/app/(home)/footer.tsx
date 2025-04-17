12a8faf3c91f0844a8584b037d236e89ad44d64f
Rishav <suranarishav08@gmail.com>
2025-04-17 11:42:54 +0530

    Navbar Mobile navbar and footer (home layout)

---
+++ src/app/(home)/footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <footer className="flex border-t justify-between items-center font-medium p-6">
+      <div>
+        <p className="flex items-center gap-2">funroad Inc.</p>
+      </div>
+    </footer>
+  );
+};
---
+++ src/app/(home)/pricing/page.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+  return (
+    <div>
+      Pricing Page
+    </div>
+  )
+}
+
+export default page
