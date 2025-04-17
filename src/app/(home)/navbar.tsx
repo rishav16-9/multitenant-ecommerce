5f0733747511fd7b220816ce7c18b8fff01597b4
Rishav <suranarishav08@gmail.com>
2025-04-17 11:51:12 +0530

    css fix in navbar

--- src/app/(home)/navbar.tsx
+++ src/app/(home)/navbar.tsx
@@ -82,7 +82,7 @@ export const Navbar = () => {
         </Button>
         <Button
           variant="secondary"
-          className="border-l border-t-0 border-b-0 border-r-0 px-12 h-full rounded-none bg-black text-white hover:bg-pink-400 hover:text-black hover: transition-colors text-lg"
+          className="border-l border-t-0 border-b-0 border-r-0 px-12 h-full rounded-none bg-black text-white hover:bg-pink-400 hover:text-black hover:transition-colors text-lg"
           asChild
         >
           <Link href="/sign-up">Start selling</Link>
