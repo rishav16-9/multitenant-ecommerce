fe48b427269e6e7218b40ec90893f2b649a63105
Rishav <suranarishav08@gmail.com>
2025-05-12 23:01:51 +0530

    Multitenant

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,10 +1,29 @@
-"use client";
+import { trpc, getQueryClient } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import type { SearchParams } from "nuqs/server";
+import { loadProductFilters } from "@/modules/products/search-params";
+import { ProductListView } from "@/modules/products/ui/views/product-list-view";
+import { DEFAULT_LIMIT } from "@/constants";
 
-import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
-
-export default function Home() {
-  const trpc = useTRPC();
-  const { data } = useQuery(trpc.auth.session.queryOptions());
-  return <div className="p-4">{JSON.stringify(data?.user, null, 2)} </div>;
+interface PageProps {
+  searchParams: Promise<SearchParams>;
 }
+export const dynamic = "force-dynamic";
+
+const Page = async ({ searchParams }: PageProps) => {
+  const filters = await loadProductFilters(searchParams);
+  const queryClient = getQueryClient();
+  void queryClient.prefetchInfiniteQuery(
+    trpc.products.getMany.infiniteQueryOptions({
+      ...filters,
+      limit: DEFAULT_LIMIT,
+    })
+  );
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <ProductListView />
+    </HydrationBoundary>
+  );
+};
+
+export default Page;
