173f484728e8bf0282a66adcb843f4178a6c84e2
Rishav <suranarishav08@gmail.com>
2025-05-15 16:20:42 +0530

    cart add and remove

--- src/modules/products/ui/views/product-view.tsx
+++ src/modules/products/ui/views/product-view.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import dynamic from "next/dynamic";
 import { StarRating } from "@/components/star-rating";
 import { Button } from "@/components/ui/button";
 import { Progress } from "@/components/ui/progress";
@@ -10,6 +10,18 @@ import { LinkIcon, StarIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { Fragment } from "react";
+// import { CartButton } from "../components/cart-button";
+const CartButton = dynamic(
+  () => import("../components/cart-button").then((mod) => mod.CartButton),
+  {
+    ssr: false,
+    loading: () => (
+      <Button disabled className="flex-1 bg-pink-400">
+        Add to cart
+      </Button>
+    ),
+  }
+);
 
 interface ProductViewProps {
   tenantSlug: string;
@@ -91,9 +103,7 @@ export const ProductView = ({ tenantSlug, productId }: ProductViewProps) => {
             <div className="border-t lg:border-t-0 lg:border-l h-full">
               <div className="flex flex-col gap-4 p-6 border-b">
                 <div className="flex flex-row items-center gap-2">
-                  <Button variant="elevated" className="flex-1 bg-pink-400">
-                    Add to cart
-                  </Button>
+                  <CartButton tenantSlug={tenantSlug} productId={productId} />
                   <Button
                     className="size-12"
                     variant="elevated"
--- src/modules/tenants/ui/components/navbar.tsx
+++ src/modules/tenants/ui/components/navbar.tsx
@@ -1,10 +1,26 @@
 "use client";
-
 import { generateTenantUrl } from "@/lib/utils";
+import dynamic from "next/dynamic";
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import Image from "next/image";
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { ShoppingCartIcon } from "lucide-react";
+const CheckoutButton = dynamic(
+  () =>
+    import("@/modules/checkout/ui/components/checkout-button").then(
+      (mod) => mod.CheckoutButton
+    ),
+  {
+    ssr: false,
+    loading: () => (
+      <Button disabled className="bg-white">
+        <ShoppingCartIcon className="text-black" />
+      </Button>
+    ),
+  }
+);
 
 interface NavbarProps {
   slug: string;
@@ -31,6 +47,7 @@ export const Navbar = ({ slug }: NavbarProps) => {
           )}
           <p className="text-xl">{data.name}</p>
         </Link>
+        <CheckoutButton hideIfEmpty tenantSlug={slug} />
       </div>
     </nav>
   );
@@ -39,7 +56,11 @@ export const Navbar = ({ slug }: NavbarProps) => {
 export const NavbarSkeleton = () => {
   return (
     <nav className="h-20 border-b font-medium bg-white">
-      <div className="max-w-(--breakpoint-xl) mx-auto flex justify-between items-center h-full  px-4 lg:px-12"></div>
+      <div className="max-w-(--breakpoint-xl) mx-auto flex justify-between items-center h-full  px-4 lg:px-12">
+        <Button disabled className="bg-white">
+          <ShoppingCartIcon className="text-black" />
+        </Button>
+      </div>
     </nav>
   );
 };
