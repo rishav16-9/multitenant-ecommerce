182385faeb228e8ca925567327a7946c0d39e397
Rishav <suranarishav08@gmail.com>
2025-06-10 09:59:39 +0530

    fix

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,9 +7,8 @@ export function cn(...inputs: ClassValue[]) {
 
 export function generateTenantUrl(tenantSlug: string) {
   const isDevelopement = process.env.NODE_ENV === "development";
-  const isSubdomainRoutingEnabled = Boolean(
-    process.env.NEXT_PUBLIC_ENABLE_SUBDOMAIN_ROUTING!
-  );
+  const isSubdomainRoutingEnabled =
+    process.env.NEXT_PUBLIC_ENABLE_SUBDOMAIN_ROUTING === "true";
   if (isDevelopement || !isSubdomainRoutingEnabled) {
     return `${process.env.NEXT_PUBLIC_APP_URL}/tenants/${tenantSlug}`;
   }
